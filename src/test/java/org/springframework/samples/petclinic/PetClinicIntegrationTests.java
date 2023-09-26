53d0c3393c28d34c328398a3053044d4ec0d0469
Kalichhe <jhondh2003@gmail.com>
2023-09-26 08:50:13 -0500

    Prueba

--- src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
@@ -55,6 +55,12 @@ public class PetClinicIntegrationTests {
 		ResponseEntity<String> result = template.exchange(RequestEntity.get("/owners/1").build(), String.class);
 		assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
+	
+	@Test
+	void testFindAll() throws Exception {
+    	assertThat(vets.findAll()).hasSize(0);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
