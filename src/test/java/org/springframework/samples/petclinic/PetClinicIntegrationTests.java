c9e112e98753bc69294327bc3e04c3b8b04c4166
Kalichhe <jhondh2003@gmail.com>
2023-09-25 20:18:56 -0500

    veamos

--- src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
@@ -55,6 +55,12 @@ public class PetClinicIntegrationTests {
 		ResponseEntity<String> result = template.exchange(RequestEntity.get("/owners/1").build(), String.class);
 		assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
+	
+	@Test
+    void testExample() {
+        // Esta aserción fallará intencionalmente
+        assertThat(1).isEqualTo(2);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
